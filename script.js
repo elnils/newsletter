7bd722636ba92ccd9abf912d77fc718dff6598cd
elnils <63009027+elnils@users.noreply.github.com>
2026-04-02 17:12:45 +0200

    script.js löschen

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('action-btn').addEventListener('click', () => {
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = "Hallo! Es funktioniert!";
-    console.log("Button wurde gedrückt.");
-});
